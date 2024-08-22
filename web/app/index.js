883843f2f98f70c518cb8b9b3bd7c87bcf14a656
Paul Kiat <pdkiat@gmail.com>
2024-08-22 15:00:47 -0700

    checkpoint app sample layout

--- web/app/index.js
+++ web/app/index.js
@@ -5,3 +5,11 @@ function $(id) {
 document.addEventListener('DOMContentLoaded', function () {
   console.log("app running!");
 })
+
+document.getElementById('circuit-board-toggle').addEventListener('click', function() {
+    document.getElementById('circuit-board-overlay').classList.toggle('active');
+});
+
+document.getElementById('close-circuit-board').addEventListener('click', function() {
+    document.getElementById('circuit-board-overlay').classList.remove('active');
+});
