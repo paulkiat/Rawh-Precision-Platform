5ae752e9cd8912b22573e8c7e85aba34dd85f100
Paul Kiat <pdkiat@gmail.com>
2024-09-02 13:03:59 -0700

    experiment with paragraph  + sentence splitting for better grouping

--- src/test/embed-match.js
+++ src/test/embed-match.js
@@ -1,8 +1,10 @@
+const { max } = require("@xenova/transformers");
 const { args, mmma } = require("../lib/util");
 const llmapis = require("../llm/api");
 const { file, model } = args;
 
 const pdf2html = require('pdf2html');
+const max_embed = 512;
 
 (async () => {
   const start = Date.now();
@@ -13,6 +15,27 @@ const pdf2html = require('pdf2html');
 
   embed.setup({ modelName: model });
   const pages = (await pdf2html.pages(file, { text: true })).map(p => clean_text(p));
+
+  // exploring chunking into paragraphs
+  const para = pages.map(p => clean_text(p)).join("\n").split("\n\n");
+  const stat = mmma(pages.map(p => p.length));
+  // const mid = para[Math.round(para.length/2)];
+  console.log({ paras: para.length, ...stat });
+
+  // switch out strategies here eg: use groupings of paragraphs instead of pages
+  // const chunk = pages
+  const ppre = para.map(p => p.length <= max_embed ? [p] : p.split(/(?=\. )/)).flat();
+  const chunks = [''];
+  for (let p of ppre) {
+    const cc = chunks[chunks.length - 1];
+    if (cc.length + p.length <= max_embed) {
+        chunks[chunks.length - 1] += "\n\n" + p;
+    } else {
+        chunks.push(p);
+    }
+  }
+  console.log({ chunks: chunks.length, ...mmma(chunks.map(c => c.length)) });
+
   const vects = await vectorize(pages);
   const qv = (await vectorize("what is the second number"))[0];
   const query = {
@@ -20,50 +43,48 @@ const pdf2html = require('pdf2html');
     index: vector_to_index(qv)
   };
 
-  const chunk = pages.map((p, i) => {
+  const embeds = chunks.map((p, i) => {
     const rec = {
       order: i,
       text: clean_text(p),
       vector: vects[i],
       index: vector_to_index(vects[i]),
     };
-    rec.coss = cosine_similarity(query, rec);
+    rec.coss = Math.abs(cosine_similarity(query, rec));
     return rec;
   });
 
-  // chunk.forEach((rec, idx) => {
+  // embeds.forEach((rec, idx) => {
   //   console.log([
   //     `-------------------------[ page ${idx + 1} ]-------------------------`,
   //     rec.text,
   //     ''
   //   ].join(''));
   // });
-
-  const para = pages.join("\n").split("\n\n");
-  const stat = mmma(pages.map(p => p.length));
-  const mid = para[Math.round(para.length/2)];
-  console.log({ paras: para.length, sample: mid, stat });
   
   // add cosine match ranking
-  const rank_coss = chunk.slice();
+  const rank_coss = embeds.slice();
   rank_coss.sort((a, b) => b.coss - a.coss).forEach((rec, idx) => {
     rec.rank_coss = idx;
   });
 
   // add cosine match ranking
-  const rank_indx = chunk.slice();
+  const rank_indx = embeds.slice();
   rank_indx.sort((a, b) => b.index - a.index).forEach((rec, idx) => {
     rec.rank_index = idx;
   });
 
-  chunk.forEach((rec, idx) => {
+  embeds.forEach((rec, idx) => {
     console.log([
-      `[ page ${(idx + 1).toString().padStart(3, ' ')} ]`,
+      `[ embed ${(idx + 1).toString().padStart(3, ' ')} ]`,
+      `[ size ${(rec.text.length).toString().padStart(4, ' ')} ]`,
       `    `,
-      `${rec.coss.toFixed(6)} | ${rec.rank_coss.toString().padStart(2, ' ')}`,
+      `${rec.coss.toFixed(6)} | ${rec.rank_coss.toString().padStart(3, ' ')}`,
       '    ',
       `${Math.abs(rec.index - query.index).toFixed(11)}`,
-      ` | ${rec.rank_index.toString().padStart(2, ' ')}`
+      ` | ${rec.rank_index.toString().padStart(3, ' ')}`,
+      rec.rank_coss === 0 ? 'C' : '',
+      rec.rank_indx === 0 ? 'I' : '',
     ].join(''));
   });
 
@@ -72,9 +93,5 @@ const pdf2html = require('pdf2html');
     match_index: chunk[rank_indx[0].order].text,
   });
 
-  console.log({
-    match_coss: chunk[rank_coss[0].order].text,
-    match_index: chunk[rank_indx[0].order].text,
-  })
-
+  console.log({ runtime: Date.now() - start });
 })();
